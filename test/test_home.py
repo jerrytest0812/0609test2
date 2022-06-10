d84073ef4ed4c4f0819cc6b3b4c4ea6306801b48
Your Name <jerryis0812@gmail.com>
2022-06-10 07:09:58 +0000

    0610 docker01

--- test/test_home.py
+++ test/test_home.py
@@ -1,5 +1,18 @@
-import requests
+# import requests
 
-def test_home():
-    r = requests.get("http://127.0.0.1:5001?a=2&b=1")
-    assert r.status_code == 200
+# def test_home():
+#     r = requests.get("http://127.0.0.1:5001?a=2&b=1")
+#     assert r.status_code == 200
+
+import pytest
+def add(x, y):
+    return x + y
+
+def test_add():
+    assert add(1, 1) == 2
+def test_add1():
+    with pytest.raises(TypeError):
+        assert add(2) == 4
+def test_add2():
+    assert(2,2) == 3
+    
